c0042e5ce81331dff2f8a61bfec6539814fa5e8f
hiroaqii <hiroaqii@gmail.com>
2016-01-04 11:19:19 +0900

    define constant and record

--- src/analog_game_checker/core.clj
+++ src/analog_game_checker/core.clj
@@ -1,6 +1,23 @@
-(ns analog-game-checker.core)
+(ns analog-game-checker.core
+  (:require [net.cgrand.enlive-html :as html]
+            [schema.core :as s]
+            [clojure.java.io :as io]
+            [clojure.string :as str]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(def twippa_url "http://twipla.jp/")
+
+(def check_text
+  ["ボードゲーム"
+   "ボドゲ"
+   "アナログゲーム"
+   "カードゲーム"
+   "非電源ゲーム"
+   "非電源系ゲーム"
+   "テーブルトークRPG"
+   "TRPG"])
+
+(s/defrecord Event
+    [url     :- s/Str
+     day     :- s/Str
+     title   :- s/Str
+     content :- s/Str])
